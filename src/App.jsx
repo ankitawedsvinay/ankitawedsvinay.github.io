913e24f94c5cb9c08d9184b3abc878a0b8b17d7a
ankitawedsvinay <vigneshsnaik.mec@gmail.com>
2024-12-01 22:23:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import WeddingDetails from "./components/WeddingDetails";
